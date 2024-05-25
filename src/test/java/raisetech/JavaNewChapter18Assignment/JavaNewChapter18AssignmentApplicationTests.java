12643dc219bc2fc5c21fcb7ba9cb18348c4d5074
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-05-26 00:24:10 +0900

    プロジェクトをセット

---
+++ src/test/java/raisetech/JavaNewChapter18Assignment/JavaNewChapter18AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.JavaNewChapter18Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNewChapter18AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
